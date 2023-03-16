2ea81f2f3fe8896d71345ac279c2ee4b91ce8f04
DemisoDaba <demo.nkmt1@gmail.com>
2023-03-16 11:51:57 -0700

    Updated

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints to string
+ * Description: Prints "and that piece of art is useful.." without puts
+ * Return: 1
+ */
+int main(void)
+{
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
+}
