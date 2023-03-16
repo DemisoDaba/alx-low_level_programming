39f8fd8a06bf0b5da05d5efdd7894d2e4fbd9c6b
DemisoDaba <demo.nkmt1@gmail.com>
2023-03-16 11:23:12 -0700

    Updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry data
+ *Description: print "\"Programming is like building a multilingual puzzle"
+ *Return:0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
