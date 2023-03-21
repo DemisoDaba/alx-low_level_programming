52b7a7fc53a3f93bd8409e3156a6ea94c272aae6
DemisoDaba <demo.nkmt1@gmail.com>
2023-03-21 06:21:27 -0700

    Updated by Demiso

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,16 @@
 
 /**
  * add - a function that adds two integers and returns the result
- * Num1: first number input
- * Num2: second number input
- * Return: Num1 + Num2
+ * @Num1: first number input
+ * @Num2: second number input
+ * Return: Sum
  */
+
 int add(int Num1, int Num2)
 {
 	int Sum;
-	Sum = (Num1 + Num2);
+
+	Sum = Num1 + Num2;
+
 	return (Sum);
 }
-
