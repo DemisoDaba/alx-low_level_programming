ebc308b7f88071f7c2cfc861d736ece2ef542809
DemisoDaba <demo.nkmt1@gmail.com>
2023-03-21 05:59:38 -0700

    Updated by Demiso

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - a function that prints the last digit of a number
- * @zack: number's last digit result
+ * @Last: number's last digit result
  * Return: value of the last digit
  */
 int print_last_digit(int Last)
