19c61cffa10d62644af288180aa1bc71e145cf49
DemisoDaba <demo.nkmt1@gmail.com>
2023-03-27 00:28:59 -0700

    Just Start

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is all about Array, Pointers and Strings
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+void swap_char(char *a, char *b);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+#endif /* MAIN_H */
