832c9c14977765c83cde4ecc26d9fead1b42984d
DemisoDaba <demo.nkmt1@gmail.com>
2023-04-10 00:00:57 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,15 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
-char **strtow(char *str);
-int count_word(char *s);
-#endif /* MAIN_H */
 
+#endif
