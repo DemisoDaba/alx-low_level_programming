c886c1309095ea6f33ecaea89206b89cbb827ce0
DemisoDaba <demo.nkmt1@gmail.com>
2023-04-12 11:12:28 -0700

    Updated

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,126 +1,91 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
+/**
+ * isNumber - checks if a string is a number
+ * @s: string to check
+ * Return: 1 if number, 0 if not
+ */
+int isNumber(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+	return (1);
+}
 
 /**
- * _is_zero - determines if any number is zero
- * @argv: argument vector.
- *
- * Return: no return.
+ * print_result - prints the result of the multiplication
+ * @result: array of integers
+ * @len: length of result
+ * Return: void
  */
-void _is_zero(char *argv[])
+void print_result(int *result, int len)
 {
-	int i, isn1 = 1, isn2 = 1;
+	int i;
 
-	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] != '0')
-		{
-			isn1 = 0;
-			break;
-		}
+	/* remove leading zeros */
+	for (i = 0; i < len && result[i] == 0; i++)
+		;
 
-	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] != '0')
-		{
-			isn2 = 0;
-			break;
-		}
+	/* if all digits are 0, print 0 */
+	if (i == len)
+		printf("0");
 
-	if (isn1 == 1 || isn2 == 1)
+	/* print the result */
+	for (; i < len; i++)
+		printf("%d", result[i]);
+
+	printf("\n");
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @ac: number of arguments
+ * @av: array of arguments
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	int i, j, num1_len, num2_len;
+	int *result;
+
+	if (ac != 3 || !isNumber(av[1]) || !isNumber(av[2]))
 	{
-		printf("0\n");
-		exit(0);
+		printf("Error\n");
+		exit(98);
 	}
-}
 
-/**
- * _initialize_array - set memery to zero in a new array
- * @ar: char array.
- * @lar: length of the char array.
- *
- * Return: pointer of a char array.
- */
-char *_initialize_array(char *ar, int lar)
-{
-	int i = 0;
+	num1_len = strlen(av[1]);
+	num2_len = strlen(av[2]);
 
-	for (i = 0; i < lar; i++)
-		ar[i] = '0';
-	ar[lar] = '\0';
-	return (ar);
-}
-
-/**
- * _checknum - determines length of the number
- * and checks if number is in base 10.
- * @argv: arguments vector.
- * @n: row of the array.
- *
- * Return: length of the number.
- */
-int _checknum(char *argv[], int n)
-{
-	int ln;
-
-	for (ln = 0; argv[n][ln]; ln++)
-		if (!isdigit(argv[n][ln]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-
-	return (ln);
-}
-
-/**
- * main - Entry point.
- * program that multiplies two positive numbers.
- * @argc: number of arguments.
- * @argv: arguments vector.
- *
- * Return: 0 - success.
- */
-int main(int argc, char *argv[])
-{
-	int ln1, ln2, lnout, add, addl, i, j, k, ca;
-	char *nout;
-
-	if (argc != 3)
-		printf("Error\n"), exit(98);
-	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
-	if (nout == NULL)
-		printf("Error\n"), exit(98);
-	nout = _initialize_array(nout, lnout);
-	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-	for (; k >= 0; k--, i--)
+	result = calloc(num1_len + num2_len, sizeof(int));
+	if (result == NULL)
 	{
-		if (i < 0)
+		printf("Error\n");
+		exit(98);
+	}
+
+	/* multiply each digit of num1 with num2 */
+	for (i = num1_len - 1; i >= 0; i--)
+	{
+		for (j = num2_len - 1; j >= 0; j--)
 		{
-			if (addl > 0)
-			{
-				add = (nout[k] - '0') + addl;
-				if (add > 9)
-					nout[k - 1] = (add / 10) + '0';
-				nout[k] = (add % 10) + '0';
-			}
-			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
-		}
-		if (j < 0)
-		{
-			if (nout[0] != '0')
-				break;
-			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-		}
-		if (j >= 0)
-		{
-			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-			addl = add / 10, nout[k] = (add % 10) + '0';
+			/* multiply each digit and add to result */
+			int mul = (av[1][i] - '0') * (av[2][j] - '0');
+
+			/* add to previous result */
+			int sum = result[i + j + 1] + mul;
+
+			/* update result */
+			result[i + j] += sum / 10;
+			result[i + j + 1] = sum % 10;
 		}
 	}
-	printf("%s\n", nout);
+
+	/* print result and free the allocated memory */
+	print_result(result, num1_len + num2_len);
+	free(result);
 	return (0);
 }
+
