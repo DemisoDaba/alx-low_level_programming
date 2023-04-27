b95c9deddec33d9ab307c636d422fe55a7c3d2ea
DemisoDaba <demo.nkmt1@gmail.com>
2023-04-27 02:34:18 -0700

    Adding Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *alis_list;
+
+	while ((alist_list = head) != NULL)
+	{
+		head = head->next;
+		free(alist_list->str);
+		free(alist_list);
+	}
+}
