dc42075356e1ccf5c9e68917095580e7c50fc07b
DemisoDaba <demo.nkmt1@gmail.com>
2023-04-27 02:15:36 -0700

    Creating Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,27 +7,30 @@
  *
  * Return: the address of the new element, or NULL if it failed.
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node = NULL;
+	list_t *new_node = NULL;
 
-    if (!head || !str)
-        return (NULL);
+	if (!head || !str)
+		return (NULL);
+	
+	new_node = malloc(sizeof(list_t));
 
-    new_node = malloc(sizeof(list_t));
-    if (!new_node)
-        return (NULL);
+	if (!new_node)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (!new_node->str)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->str = strdup(str);
 
-    new_node->len = strlen(str);
-    new_node->next = *head;
-    *head = new_node;
+	if (!new_node->str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    return (new_node);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	
+	return (new_node);
 }
