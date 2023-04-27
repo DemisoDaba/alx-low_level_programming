dfb11b8de232eefaacee92d243a84c16a07bfc8d
DemisoDaba <demo.nkmt1@gmail.com>
2023-04-27 01:45:56 -0700

    Creating Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		num_nodes++;
+		h = h->next;
+	}
+	return (num_nodes);
+}
